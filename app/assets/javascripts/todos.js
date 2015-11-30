9965d364e21784057bfa7eaa3c3abf68268a808d
Avidor Turkewitz <aviturkewitz@gmail.com>
2015-11-30 16:32:43 -0500

    Working with AJAX

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -5,6 +5,19 @@ $(function(){
 
     var action = $(this).attr('action');
     var method = $(this).attr('method');
-    
+
+    var description = $(this).find('#todo_description').val();
+    var priority = $(this).find('#todo_priority').val();
+
+    var data = $(this).serializeArray();
+
+    $.ajax({
+      method: method,
+      url: action,
+      data: data,
+
+      dataType: 'script'
+    })
+
   })
 });
